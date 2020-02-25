67f9c0515f42113009397e75165b627a7cecd203
aeroju <aeroju@sina.com>
2020-02-25 17:49:36 +0800

    add tcp punch hole

---
+++ tcp_client.py
@@ -0,0 +1,93 @@
+import socket
+import threading
+import time
+import logging
+
+from .message_wrapper import *
+logger = logging.getLogger('tcp_client')
+logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(message)s')
+
+
+class TcpClient(object):
+    def __init__(self,server_addr,server_port,timeout=5):
+        self.server = (server_addr,server_port)
+        self.timeout = timeout
+
+    def _local_server_hanlder(self,conn):
+        while not self.stop_event.is_set():
+            try:
+                data = conn.recv(1024)
+                command,msg = de_wapper(data)
+                logger.info('message from peer: %d,%s',command,msg.__str__())
+            except socket.timeout:
+                continue
+        conn.close()
+
+    def _accept(self,port):
+        logger.info('starting local server on port %d for peer to connect...',port)
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        sock.bind(('',port))
+        sock.listen(1)
+        sock.settimeout(self.timeout)
+        while(not self.stop_event.is_set()):
+            try:
+                conn,addr = sock.accept()
+                conn.settimeout(self.timeout)
+                server_thread = threading.Thread(target=self._local_server_hanlder,args=(conn))
+            except socket.timeout:
+                continue
+        sock.close()
+
+    def _connect(self,local,peer):
+        logger.info('begin to connect to peer:%s:%d',peer)
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        sock.bind(local)
+        while not self.stop_event.is_set():
+            try:
+                sock.connect(peer)
+                sock.send(wapper(COMMAND_TEXT,{'msg':'hello'}))
+            except socket.error:
+                continue
+
+
+    def run(self,port = 1234,key = 100):
+        self.fsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.fsock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        self.fsock.connect(self.server)
+        self.local_addr = self.fsock.getsockname()
+
+        self.stop_event = threading.Event()
+
+        logger.info('connect to server and regist for peer key %s',key)
+        msg = {'local_addr':self.local_addr,'peer_key':key}
+        self.fsock.send(wapper(COMMAND_SIGN,msg))
+
+        data = self.fsock.recv(1024)
+        command,msg = de_wapper(data)
+        if(command==COMMAND_SIGN_ACK):
+            self.public_addr = msg['public_addr']
+            logger.info('public address: %s:%d',self.public_addr)
+            self.local_server_thread = threading.Thread(target=self._accept,args=(self.local_addr[1]))
+            self.local_server_thread.start()
+
+        logger.info('requesting peer...')
+        self.fsock.send(wapper(COMMAND_REQUEST_PEER,{'peer_key':key}))
+        data = self.fsock.recv(1024)
+        command,msg = de_wapper(data)
+        if(command==COMMAND_REQUEST_PEER_ACK):
+            peers = msg['peers']
+            logger.info('peers: %s',peers.__str__())
+            self.peers_thread=[]
+            for peer in peers:
+                peer_thread = threading.Thread(target=self._connect,args=(self.local_addr,peer))
+                self.peers_thread.append(peer_thread)
+                peer_thread.start()
+
+
+if __name__ == '__main__':
+    TcpClient('13.115.178.224',12345).run(key=100)
+
---
+++ tcp_server.py
@@ -0,0 +1,93 @@
+import socket
+import logging
+import threading
+import time
+
+from .message_wrapper import *
+logger = logging.getLogger('tcp_server')
+logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(message)s')
+
+class TcpServer(object):
+    def __init__(self,port):
+        self.port = port
+        self.clients={}
+        self.peers = {}
+
+    def get_client(self,addr):
+        key = addr[0] + ':' + addr[1]
+        return self.clients.get(key)
+
+    def set_client(self,addr,client_info):
+        key = addr[0] + ':' + addr[1]
+        self.clients[key] = client_info
+
+    def _client_handler(self,conn,client_addr,stop_event):
+        while(not stop_event.is_set()):
+            try:
+                data = conn.recv(1024)
+                command,msg = de_wapper(data)
+                logger.info('from client command: %d msg: %s ', command,msg.__str__())
+                if(command==COMMAND_SIGN):
+                    client_local_addr = msg['local_addr']
+                    peer_key = msg['peer_key']
+                    client_info = self.get_client(client_addr)
+                    client_info['local_addr'] = client_local_addr
+                    client_info['peer_key'] = peer_key
+                    self.set_client(client_addr,client_info)
+                    msg = {'public_addr':client_addr}
+                    logger.info('message send to client:%s', msg.__str__())
+                    conn.send(wapper(COMMAND_SIGN_ACK,msg))
+                elif(command==COMMAND_REQUEST_PEER):
+                    peer_key = msg['peer_key']
+                    peers = []
+                    for key,item in self.clients:
+                        if(item.get('peer_key') is not None and item.get('peer_key')==peer_key):
+                            if(key!=client_addr):
+                                peers.append(item.get('public_addr'))
+                    msg={'peers':peers}
+                    logger.info('message send to client:%s', msg.__str__())
+                    conn.send(wapper(COMMAND_REQUEST_PEER_ACK,msg))
+
+            except socket.timeout:
+                conn.close()
+                break
+
+    def _accept(self,fsock,stop_event):
+        while(not stop_event.is_set()):
+            try:
+                conn,addr = fsock.accept()
+            except socket.timeout:
+                continue
+            logger.info('connection from: %s:%d',addr)
+            client_thread = threading.Thread(target=self._client_handler,args=(conn,addr,stop_event))
+            client_info = {}
+            client_info['thread'] = client_thread
+            client_info['public_addr'] = addr
+            self.set_client(addr,client_info)
+            client_thread.start()
+
+
+    def run(self):
+        self.stop_event = threading.Event()
+
+        self.fsock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.fsock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        self.fsock.bind(('0.0.0.0',self.port))
+        self.fsock.listen(1)
+        self.fsock.settimeout(2)
+        logger.info('server start at %d' , self.port)
+
+        self.run_thread =  threading.Thread(target=self._accept,args=(self.fsock,self.stop_event,))
+        self.run_thread.start()
+
+    def stop(self):
+        self.stop_event.set()
+        time.sleep(1)
+        self.fsock.close()
+
+if __name__ == '__main__':
+    server = TcpServer(12345)
+    server.run()
+    input()
+    logger.info('waiting connections to close')
+    server.stop()
